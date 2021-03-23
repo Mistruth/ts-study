e15a1c967a5e0ca624e013be915759a89f87b4f7
Mistruth <kikizxc4@qq.com>
2021-03-23 21:41:29 +0800

    add:17

---
+++ questions/17-lookup/index.ts
@@ -0,0 +1,1 @@
+export type LookUp<U extends { type: any }, T extends U['type']> = U extends { type: T } ? U : never;
---
+++ questions/17-lookup/test-cases.ts
@@ -0,0 +1,23 @@
+import type { Equal, Expect } from '../../utils'
+import type { LookUp } from './index'
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
+
+
+type A = LookUp<Animal, 'dog'>
+
+type cases = [
+  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+]
